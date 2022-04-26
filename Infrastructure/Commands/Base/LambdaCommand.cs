e5b24dae4b6fab1c0431f33a31ed7b4d06c9748b
Иван Сугробов <i.a.sugrobov@mgutm.ru>
2022-04-26 23:27:41 +0300

    Подключение пакетов, разбиение на папки для MVVM, создание начального шаблона, создание базовых классов Команды и Модели-представления

---
+++ Infrastructure/Commands/Base/LambdaCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourceProjectMVVMAndEntityFramework.Infrastructure.Commands.Base
+{
+    class LambdaCommand : BaseCommand
+    {
+        private Action<object> CommandExecute;
+        private Func<object, bool> CanCommandExecuted;
+
+        public LambdaCommand(Action<object> CommandExec, Func<object, bool> CanCommandExec = null)
+        {
+            CommandExecute = CommandExec ?? throw new ArgumentNullException(nameof(CommandExec));
+            CanCommandExecuted = CanCommandExec;
+        }
+
+        public override bool CanExecute(object parameter) => CanCommandExecuted?.Invoke(parameter) ?? true;
+
+        public override void Execute(object parameter) => CommandExecute(parameter);
+    }
+}
