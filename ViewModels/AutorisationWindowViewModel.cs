f14953c9f21c88bf086c79ddec2cfd6aaa6889d4
Иван Сугробов <i.a.sugrobov@mgutm.ru>
2022-05-06 10:37:51 +0300

    Добавление MaterialDesign. Добавление окна входа/регистрации.

---
+++ ViewModels/AutorisationWindowViewModel.cs
@@ -0,0 +1,69 @@
+﻿using CourceProjectMVVMAndEntityFramework.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourceProjectMVVMAndEntityFramework.ViewModels
+{
+    class AutorisationWindowViewModel : BaseViewModel
+    {
+        #region Логин авторизации
+
+        private string _ALogin;
+        public string ALogin
+        {
+            get => _ALogin;
+            set => Set(ref _ALogin, value);
+        }
+
+        #endregion
+
+        #region Пароль авторизации
+
+        private string _APassword;
+        public string APassword
+        {
+            get => _APassword;
+            set => Set(ref _APassword, value);
+        }
+
+        #endregion
+
+        #region Логин регистрации
+
+        private string _RLogin;
+        public string RLogin
+        {
+            get => _RLogin;
+            set => Set(ref _RLogin, value);
+        }
+
+        #endregion
+      
+        #region Первый пароль регистрации
+
+        private string _RFPassword;
+        public string RFPassword
+        {
+            get => _RFPassword;
+            set => Set(ref _RFPassword, value);
+        }
+
+        #endregion
+
+        #region Второй пароль регистрации
+
+        private string _RSPassword;
+        public string RSPassword
+        {
+            get => _RSPassword;
+            set => Set(ref _RSPassword, value);
+        }
+
+        #endregion
+
+
+    }
+}
